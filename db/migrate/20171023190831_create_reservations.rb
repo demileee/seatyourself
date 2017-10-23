e9011bd1a8f39a59c243372930a3d0b4af4cc0e7
Argir Argirov <dj.giro@gmail.com>
2017-10-23 15:22:10 -0400

    models, migrations, controllers, empty views is generated

---
+++ db/migrate/20171023190831_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reservations do |t|
+      t.datetime :date_time
+      t.string :name
+      t.string :email
+      t.text :notes
+      t.integer :restaurant_id
+
+      t.timestamps
+    end
+  end
+end
