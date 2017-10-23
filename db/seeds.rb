9f30af12f74061fece494a70e3d0d85cd9d762a0
horaciocoronel <arielc@gmail.com>
2017-10-23 15:38:38 -0400

    Add Restaurant data to db:seeds file)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.create(name: 'Tai me Tail', address: '44 Queen St. West | Toronto ON', description: 'Great Tai Restaurant')
+Restaurant.create(name: 'Burrito Loco', address: '344 King St. West | Toronto ON', description: 'Great Mexican food')
+Restaurant.create(name: 'Asahi Restaurant', address: '192 Yonge St. | Toronto ON', description: 'Sushi food restaurant')
+Restaurant.create(name: 'Cooper\'s', address: '444 Adelaide St. | Toronto ON', description: 'Brunch Restaurant')
+Restaurant.create(name: 'Hal Tal Mal', address: '916 King St. East | Toronto ON', description: 'Exquisite Indian Cuisine')
+Restaurant.create(name: 'Shopska', address: '692 Bloor St. W | Toronto ON', description: 'Bulgarian Food')
+Restaurant.create(name: 'Che, Boludo', address: '1992 Wilson St.  | Toronto ON', description: 'Argentinean food with a side of Italian')
+Restaurant.create(name: 'Pastamonium', address: '998 Bloor St. W | Toronto ON', description: 'Great Italian Food')
