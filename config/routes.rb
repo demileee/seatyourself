7db042fe3f2450bd9837e50414c91ed04ba8496e
Argir Argirov <dj.giro@gmail.com>
2017-10-24 12:11:09 -0400

    user login page, resources, index page links, sessions controller and methods,

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reservations
   end
+  resource :sessions, only: [:create, :new, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
