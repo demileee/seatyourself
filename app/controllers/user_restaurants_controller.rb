cf02a52874973fff07b6d5071a0e60d6d8a070e6
horaciocoronel <arielc@gmail.com>
2017-10-25 16:38:26 -0400

    Add style to buttons on all my restaurants

--- app/controllers/user_restaurants_controller.rb
+++ app/controllers/user_restaurants_controller.rb
@@ -2,7 +2,7 @@ class UserRestaurantsController < ApplicationController
 
   def index
     ensure_logged_in
-    @restaurants = Restaurant.where("user_id = ?", params[:user_id])
+    @restaurants = Restaurant.where("user_id = ?", params[:user_id]).order(:name)
 
     if current_user.id != params[:user_id].to_i
       flash[:alert] = 'Not authorized user'
