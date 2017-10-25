4474ed14e727d029a095467a43bf818a98e171d6
Demi Lee <demilee.to@gmail.com>
2017-10-25 12:31:05 -0400

    edit and delete user reservations, fix helper method for datetime

--- app/controllers/user_reservations_controller.rb
+++ app/controllers/user_reservations_controller.rb
@@ -7,4 +7,37 @@ include ApplicationHelper
     @reservations = @user.reservations.all
   end
 
+  def edit
+    @user = User.find(params[:user_id])
+    @reservation = @user.reservations.find(params[:id])
+
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @reservation = @user.reservations.find(params[:id])
+    @reservation.update_attributes(reservation_params)
+    @reservation.date_time = datetime(params[:res][:date], params[:res]["time(4i)"], params[:res]["time(5i)"])
+    if @reservation.save
+      flash[:success] = 'Reservation updated'
+      redirect_to edit_user_user_reservation_path(params[:user_id], params[:id])
+    else
+      flash[:alert] = 'Please verify the information'
+      redirect_to edit_user_user_reservation_path(params[:user_id], params[:id])
+    end
+
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @reservation = @user.reservations.find(params[:id])
+    @reservation.destroy
+    redirect_to user_user_reservations_path(params[:user_id])
+  end
+
+  private
+  def reservation_params
+    params.require("res").permit(:name, :email, :notes)
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,34 @@
 module ApplicationHelper
-  
+
   def datetime(date, hour, min)
     d = Date.parse(date) if date != ''
-    time = []
-    time << hour
-    time << min
-    time.join(" ")
-    t = Time.parse(time[0])
-    dt = DateTime.new(d.year, d.month, d.day, t.hour, t.min, t.sec, t.zone) if date != ''
+    dt = DateTime.new(d.year, d.month, d.day, hour.to_i, min.to_i) if date != ''
     return dt
   end
 
   def dateformat(date)
     d = date.localtime.strftime("%A, %d %b %Y %l:%M %p")
     return d
+  end
+
+  def fulldate(date_time)
+    d = date_time.localtime.strftime("%Y-%m-%d")
+    return d
+  end
+
+  def hour(date_time)
+    h = date_time.localtime.strftime("%H")
+    return h
+  end
+
+  def min(date_time)
+    m = date_time.locatime.strftime("%M")
+    return m
+  end
+
+  def hourmin(date_time)
+    hm = date_time.localtime.strftime("%H:%M")
+    return hm
   end
 
 end
